e80981c85d0c40f215b70bfc66f58264d0841020
mthiagu <thiagu.m@gmail.com>
2021-05-25 21:01:56 +0530

    Fixing test.pl changes

--- test.pl
+++ test.pl
@@ -6,13 +6,6 @@ use strict;
 
 print "Program starts here";
 print "Hello World";
-#Test program
-
-use strict;
-use warnings;
-
-print "Program starts here";
-print "Hello World";
 print "Program ends here";
 print "Rock";
 print "World";
