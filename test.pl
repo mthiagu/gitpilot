128905bb37066c87bb1730dd89764498ab7a765d
mthiagu <thiagu.m@gmail.com>
2021-05-26 13:12:42 +0530

    Adding changes for grepping opt numbers

--- test.pl
+++ test.pl
@@ -10,3 +10,14 @@ print "Program ends here";
 print "Rock";
 print "World";
 print "Have Peave";
+
+open(FH,"input.txt");#dirent.h#include file stdlib.h
+while(<FH>)
+{
+#print "$_\n";
+$line = $_;
+$line = ~ /include file(.*)/si;
+print "Change $1 \n";
+}
+#$text = $line ;
+#$text =~ /OPT.*?(\d{4,7})/si;
